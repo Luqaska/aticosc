c22ec11eccb5d8a75b13495fb19ded54e6c02bc8
LucasSoft LucasNT 3.1 <83518257+luqaska@users.noreply.github.com>
2022-02-04 21:50:18 +0000

    Create index.php

---
+++ index.php
@@ -0,0 +1,37 @@
+<?php include("_inc.php") ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+<head>
+<style>
+body {
+	text-align: justify;
+	background: black;
+	color: white;
+	margin: 5px 22%;
+}
+header {
+	border-radius: 10px 10px 0 0;
+	background: white;
+	color: black;
+}
+main {
+	border: solid 1px white;
+	border-radius: 0 0 10px 10px;
+}
+</style>
+<title>LuqaScape</title>
+</head>
+<body>
+<header>
+  <h1>ATICO<span style="font-size:small">.0rg.us</span></h1>
+</header>
+<main>
+<p>Una carta de amor al viejo Internet.<br>
+A love letter to the old Internet.</p>
+<br>
+<ul>
+<li><a href="cube.htm">Netscape Cube</a></li>
+</ul>
+</main>
+</body>
+</html>
