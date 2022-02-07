f4a7e00826e19208b25feecf90ef76c64e5e9a91
LucasSoft LucasNT 3.1 <83518257+luqaska@users.noreply.github.com>
2022-02-07 18:36:07 +0000

    Create blog.php

---
+++ blog.php
@@ -0,0 +1,8 @@
+<?php $p="Home"; include("_inc.php"); ?>
+<main>
+<h1>Bitácora de Lucas</h1>
+<iframe style="border:0" src="https://atilog.lu700.repl.co" width="100%" height="560px"></iframe>
+<hr />
+<a rel="license" href="https://creativecommons.org/licenses/by/4.0/"><img alt="Licencia Creative Commons" style="border-width:0" src="https://i.creativecommons.org/l/by/4.0/88x31.png" /></a>
+</main>
+<?php include("_last.php");
