9a9e58d28507af7017a9cce5e872f53173b6c242
LucasSoft LucasNT 3.1 <83518257+luqaska@users.noreply.github.com>
2022-02-07 18:21:15 +0000

    Update and rename _inc.php to includes/start.php

---
+++ includes/start.php
@@ -0,0 +1,12 @@
+<?php session_start();
+if(!isset($_SESSION["logged"])){
+  if(isset($_POST["code"])){
+    if($_POST["code"]=="scraft7769"){
+      $_SESSION["logged"]=true;
+    }else{
+      header("?error");
+    }
+  }else{
+    die('<!DOCTYPE html><html><body><form method="POST"><input type="password" name="code"><input type="submit"><br>(Es la misma que SCCJ)</form></body></html>');
+  }
+}
