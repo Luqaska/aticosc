60ae8100115e87b6a28ad9a553c6a574daceacf8
LucasSoft LucasNT 3.1 <83518257+luqaska@users.noreply.github.com>
2022-02-07 18:29:08 +0000

    Update _inc.php

--- _inc.php
+++ _inc.php
@@ -1,16 +1,4 @@
-<?php session_start();
-if(!isset($_SESSION["logged"])){
-	if(isset($_POST["code"])){
-		if($_POST["code"]=="scraft7769"){
-			$_SESSION["logged"]=true;
-		}else{
-			header("?error");
-		}
-	}else{
-		die('<!DOCTYPE html><html><body><form method="POST"><input type="password" name="code"><input type="submit"><br>(Es la misma que SCCJ)</form></body></html>');
-	}
-}else{
-$pages_nav = [
+<?php $pages_nav = [
 	["Home", "index.php"],
 	["Logs", "blog.php"]
 ]; ?>
@@ -113,4 +101,3 @@ footer {
 	echo "$b[0]<a href=\"$element[1]\">$element[0]</a>$b[1]";
 } ?>
 </nav>
-<?php }
