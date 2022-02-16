a82d50d9c998d69d4e356793ea8fc461ac8d9518
LucasSoft LucasNT 3.1 <83518257+luqaska@users.noreply.github.com>
2022-02-15 23:13:31 -0300

    Create board.php

---
+++ board.php
@@ -0,0 +1,7 @@
+<?php $p="Muro"; include("_inc.php"); ?>
+<main>
+<h1>Muro</h1>
+<p>Poné lo que quieras</p>
+<iframe style="border:0" src="https://aticom.lu700.repl.co/?s=muro" width="100%" height="560px"></iframe>
+</main>
+<?php include("_last.php");
