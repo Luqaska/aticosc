c45a08a86fa35d1cd81e025399d25949e9fe33a7
Lucas <83518257+luqaska@users.noreply.github.com>
2022-03-10 13:14:28 -0300

    Update board.php

--- board.php
+++ board.php
@@ -2,6 +2,6 @@
 <main>
 <h1>Muro</h1>
 <p>Poné lo que quieras</p>
-<iframe style="border:0" src="https://aticom.lu700.repl.co/?s=muro" width="100%" height="560px"></iframe>
+<iframe style="border:0" src="https://com.atico.ga/?s=muro" width="100%" height="560px"></iframe>
 </main>
 <?php include("_last.php");
