ba43724fe75d3774a586dc39203c7b040ba4a0f3
Lucas <83518257+luqaska@users.noreply.github.com>
2022-03-10 13:15:37 -0300

    Update blog.php

--- blog.php
+++ blog.php
@@ -3,7 +3,7 @@
 <h1>Bitácora de Lucas</h1>
 <iframe style="border:0" src="https://atilog.lu700.repl.co" width="100%" height="560px"></iframe>
 <h3>Comentarios</h3>
-<iframe style="border:0" src="https://aticom.lu700.repl.co/?s=blog" width="100%" height="150px"></iframe>
+<iframe style="border:0" src="https://com.atico.ga/?s=blog" width="100%" height="150px"></iframe>
 <hr />
 <a rel="license" href="https://creativecommons.org/licenses/by/4.0/"><img alt="Licencia Creative Commons" style="border-width:0" src="https://i.creativecommons.org/l/by/4.0/88x31.png" /></a>
 </main>
